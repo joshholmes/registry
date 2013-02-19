925fd1cb9de9abbf9d268e2ef6c45f0c9427b359
Tim Park <timfpark@gmail.com>
2013-02-18 19:59:32 -0800

    need to specifically call out Content-Type, stars not allowed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
   // everything is JSON out of magenta
 
