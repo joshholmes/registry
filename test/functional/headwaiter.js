a39914479512893fb4191d4fe6829c3ade432a1a
Tim Park <timfpark@gmail.com>
2013-03-29 21:07:16 -0700

    surround headwaiter json results with endpoints

--- test/functional/headwaiter.js
+++ test/functional/headwaiter.js
@@ -5,13 +5,13 @@ var assert = require('assert')
 
 describe('headwaiter endpoint', function() {
     it('should return service endpoints json', function(done) {
-        request(config.base_url + '/headwaiter', function(err,resp,body) {
+        request.get({url: config.base_url + '/headwaiter', json: true}, function(err,resp,body) {
             assert.equal(resp.statusCode, 200);
 
-            var endpointJSON = JSON.parse(body);
-            assert.ok(utils.stringEndsWith(endpointJSON.blobs_endpoint, "/blobs/"));
-            assert.ok(utils.stringEndsWith(endpointJSON.messages_endpoint, "/messages/"));
-            assert.ok(utils.stringEndsWith(endpointJSON.principals_endpoint, "/principals/"));
+            assert.notEqual(body.endpoints, undefined);
+            assert.equal(utils.stringEndsWith(body.endpoints.blobs_endpoint, "/blobs/"), true);
+            assert.equal(utils.stringEndsWith(body.endpoints.messages_endpoint, "/messages/"), true);
+            assert.equal(utils.stringEndsWith(body.endpoints.principals_endpoint, "/principals/"), true);
 
             done();
         });
