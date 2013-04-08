50390009f549f4fcd5914ae676984828c4fac6b0
Tim Park <timfpark@gmail.com>
2013-04-08 06:08:15 -0700

    allow authorization header

--- middleware/crossOrigin.js
+++ middleware/crossOrigin.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     // Everything out of magenta is JSON
     res.setHeader('Content-Type', 'application/json');
