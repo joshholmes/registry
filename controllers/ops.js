b539a8a0d4e1101d9719b04182cd54dcc4aaf59a
Tim Park <timfpark@gmail.com>
2013-04-10 19:40:08 -0700

    add remoteAddress to ops/health

--- controllers/ops.js
+++ controllers/ops.js
@@ -1,14 +1,16 @@
 var config = require('../config')
-  , services = require('../services');
+  , services = require('../services')
+  , utils = require('../utils');
 
 exports.health = function(req, res) {
     var status = "ok";
     services.messages.find({}, 0, 1, null, function(err, messages) {
         if (err) status = "failing";
 
-        res.send({status: status,
-                  memory: process.memoryUsage(),
-                  pid: process.pid,
-                  uptime: process.uptime()});
+        res.send({ status: status,
+                   memory: process.memoryUsage(),
+                   remoteAddress: utils.ipFromRequest(req),
+                   pid: process.pid,
+                   uptime: process.uptime() });
     });
 };
