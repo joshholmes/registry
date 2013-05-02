6e70658bd1c8fda17483eaa57b83d9259a84d203
Tim Park <timfpark@gmail.com>
2013-05-01 20:26:52 -0700

    add test for claiming device

--- test/unit/agents.js
+++ test/unit/agents.js
@@ -21,7 +21,6 @@ describe('agent service', function() {
     });
 
     it('deviceMatching does not match 2 users at same ip address for 2nd user', function(done) {
-
         services.principals.updateLastConnection(fixtures.models.principals.user, "127.0.0.1");
         services.principals.updateLastConnection(fixtures.models.principals.anotherUser, "127.0.0.1");
 
@@ -32,6 +31,44 @@ describe('agent service', function() {
                 assert.ifError(err);
                 assert.equal(messages.length > 0, false);
                 done();
+            });
+        }, 200);
+
+    });
+
+    it('claiming a device changes the owner', function(done) {
+        services.principals.updateLastConnection(fixtures.models.principals.user, "127.0.0.1");
+
+        // wait for matcher agent to match the user and device ip addresses.
+        setTimeout(function() {
+            services.messages.find(services.principals.systemPrincipal, { message_type: "ip_match" }, {}, function(err, messages) {
+                assert.ifError(err);
+                assert.equal(messages.length > 0, true);
+
+                var ipMatch = messages[0];
+
+                var claimMessage = models.Message({
+                    message_type: 'claim',
+                    response_to: ipMatch.id,
+                    from: fixtures.models.principals.user.id,
+                    to: services.principals.systemPrincipal.id,
+                    body: {
+                        principal: ipMatch.body.principal
+                    }
+                });
+
+                services.messages.create(claimMessage, function(err, message) {
+                    assert.ifError(err);
+
+                    // wait for claim agent to assign the device to the user.
+                    setTimeout(function() {
+                        services.principals.findById(services.principals.systemPrincipal, ipMatch.body.principal, function(err, principal) {
+                            assert.ifError(err);
+                            assert.equal(principal.owner, fixtures.models.principals.user.id);
+                            done();
+                        });
+                    }, 200);
+                });
             });
         }, 200);
 
