d757085c8a747297a167694c198b4675639bc235
Tim Park <timfpark@gmail.com>
2014-05-11 22:23:45 -0700

    remove capabilities

--- models/principal.js
+++ models/principal.js
@@ -15,9 +15,6 @@ principalSchema.add({
     last_ip:         { type: String },
     last_connection: { type: Date, default: Date.now },
 
-    // OBSOLETE - remove after migration 00004 has run.
-    capabilities:    { type: Array },
-
     tags:            [{ type: String }],
 
 // non-user fields
