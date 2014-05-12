66e4b28c38ac0b3174cc492e78557c70aae16d69
Tim Park <timfpark@gmail.com>
2014-05-11 21:33:57 -0700

    remove obsolete migrations

--- migrations/00001-add-permissions.js
+++ migrations/00001-add-permissions.js
@@ -5,28 +5,36 @@ var async = require('async')
 exports.up = function(callback) {
     var permissions = [
         // disallow anyone else from sending 'ip' messages.
-        services.permissions.translate({ 
-            action: 'send', 
-            filter: '{ "type": "ip" }', 
-            authorized: false, 
-            priority: 550 
+        services.permissions.translate({
+            action: 'send',
+            filter: '{ "type": "ip" }',
+            authorized: false,
+            priority: 550
         }),
 
         /* userland permissions exist between priority 1M-2B */
 
         // if no other higher priority rules allow it, do not allow messages to be sent to: a principal.
         services.permissions.translate({
-            action: 'send', 
-            filter: '{ "to": { "$ne": null } }', 
-            authorized: false, 
-            priority: models.Permission.DEFAULT_PRIORITY_BASE + 500 
+            action: 'send',
+            filter: '{ "to": { "$ne": null } }',
+            authorized: false,
+            priority: models.Permission.DEFAULT_PRIORITY_BASE + 500
         }),
 
         // if no other higher priority rules forbid it, allow messages to be sent.
         services.permissions.translate({
-            action: 'send', 
-            authorized: true, 
+            action: 'send',
+            authorized: true,
             priority: models.Permission.DEFAULT_PRIORITY_BASE + 1000
+        }),
+
+        // all principals are allowed to message the service
+        services.permissions.translate({
+            principal_for: 'service',
+            action: 'send',
+            authorized: true,
+            priority: 510
         })
     ];
 
